bf7ed573df7f8bcd08867d5901d01b45084af0cb
nirajkalra <32154629+nirajkalra@users.noreply.github.com>
2018-04-17 22:24:51 +0200

    Update RSAkeygen.java

--- applet/test/java/RSAkeygen.java
+++ applet/test/java/RSAkeygen.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package simpleapdu;
+package applet;
 
 import java.util.Arrays;
 import javacard.framework.*;
